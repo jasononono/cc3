82b433f4bd0b4a31ee5b93dfa9bf82dd88100df7
jasononono <cdliaowendi@gmail.com>
2025-09-28 13:19:14 -0400

    list matrix separation

--- py/cc3/graph.py
+++ py/cc3/graph.py
@@ -9,7 +9,7 @@ class Edge:
         return self.dest == other.dest and self.weight == other.weight
 
 
-class Graph:
+class ListGraph:
     def __init__(self, v = 0, weighted = False, directed = False):
         self.order = v # n
         self.size = 0 # e
@@ -17,29 +17,18 @@ class Graph:
         self.weighted = weighted
         self.directed = directed
 
-        self.matrix: list[list[int]] = [[0] * v for _ in range(v)]
         self.list: list[list[Edge]] = [[] for _ in range(v)]
 
     # ADD VERTICES
     def add_vertices(self, amount = 1):
         self.order += amount
-
-        for i in self.matrix:
-            i.extend([0] * amount)
-        self.matrix.extend([[0] * self.order for _ in range(amount)])
-
         self.list.extend([[] for _ in range(amount)])
 
     # IS EDGE
-    def is_edge(self, a, b):
+    def is_edge(self, a, b): # O(degree of a)
         if a >= self.order or b >= self.order:
             return False
-        return self.is_edge_matrix(a, b)
 
-    def is_edge_matrix(self, a, b): # O(1)
-        return self.matrix[a][b] != 0
-
-    def is_edge_list(self, a, b): # O(degree of a)
         for e in self.list[a]:
             if e == b:
                 return True
@@ -52,17 +41,15 @@ class Graph:
 
         if a >= self.order or b >= self.order:
             self.add_vertices(max(a, b) - self.order + 1)
+        if not self.weighted:
+            w = 1
 
-        self._add_edge(a, b, w)
+        self.list[a].append(Edge(b, w))
         if not self.directed:
-            self._add_edge(b, a, w)
+            self.list[b].append(Edge(a, w))
 
         self.size += 1
         return True
-
-    def _add_edge(self, a, b, w):
-        self.matrix[a][b] = w if self.weighted else 1
-        self.list[a].append(Edge(b, w if self.weighted else 1))
 
     # REMOVE EDGE
     def remove_edge(self, a, b):
@@ -74,7 +61,6 @@ class Graph:
         return result
 
     def _remove_edge(self, a, b):
-        self.matrix[a][b] = 0
         for i, e in enumerate(self.list[a]):
             if e == b:
                 del self.list[a][i]
@@ -82,41 +68,103 @@ class Graph:
         return False
 
     # OUT DEGREE
-    def out_degree(self, v):
+    def out_degree(self, v): # O(1)
         if v >= self.order:
             return -1
-        return self.out_degree_list(v)
+        return len(self.list[v])
 
-    def out_degree_matrix(self, v): # O(n)
+    # IN DEGREE
+    def in_degree(self, v): # O(e)
+        if v >= self.order:
+            return -1
+
+        count = 0
+        for i in self.list:
+            for e in i:
+                if e == v:
+                    count += 1
+        return count
+
+    # DEGREE
+    def degree(self, v):
+        if v >= self.order:
+            return -1
+        return (self.in_degree(v) if self.directed else 0) + self.out_degree(v)
+
+
+class MatrixGraph:
+    def __init__(self, v = 0, weighted = False, directed = False):
+        self.order = v  # n
+        self.size = 0  # e
+
+        self.weighted = weighted
+        self.directed = directed
+
+        self.matrix: list[list[int]] = [[0] * v for _ in range(v)]
+
+    # ADD VERTICES
+    def add_vertices(self, amount = 1):
+        self.order += amount
+
+        for i in self.matrix:
+            i.extend([0] * amount)
+        self.matrix.extend([[0] * self.order for _ in range(amount)])
+
+    # IS EDGE
+    def is_edge(self, a, b): # O(1)
+        if a >= self.order or b >= self.order:
+            return False
+        return self.matrix[a][b] != 0
+
+    # ADD EDGE
+    def add_edge(self, a, b, w = 1):
+        if self.is_edge(a, b):
+            return False
+
+        if a >= self.order or b >= self.order:
+            self.add_vertices(max(a, b) - self.order + 1)
+        if not self.weighted:
+            w = 1
+
+        self.matrix[a][b] = w
+        if not self.directed:
+            self.matrix[b][a] = w
+
+        self.size += 1
+        return True
+
+    # REMOVE EDGE
+    def remove_edge(self, a, b):
+        if self.matrix[a][b] == 0:
+            return False
+
+        self.matrix[a][b] = 0
+        if not self.directed:
+            self.matrix[b][a] = 0
+
+        self.size -= 1
+        return True
+
+    # OUT DEGREE
+    def out_degree(self, v):  # O(n)
+        if v >= self.order:
+            return -1
+
         count = 0
         for i in self.matrix[v]:
             if i:
                 count += 1
         return count
 
-    def out_degree_list(self, v): # O(1)
-        return len(self.list[v])
-
     # IN DEGREE
-    def in_degree(self, v):
+    def in_degree(self, v):  # O(n)
         if v >= self.order:
             return -1
-        return (self.in_degree_matrix(v) if self.order < self.size else
-                self.in_degree_list(v))
 
-    def in_degree_matrix(self, v): # O(n)
         count = 0
         for i in range(self.order):
             if self.matrix[i][v]:
                 count += 1
-        return count
-
-    def in_degree_list(self, v): # O(e)
-        count = 0
-        for i in self.list:
-            for e in i:
-                if e == v:
-                    count += 1
         return count
 
     # DEGREE
--- py/cc3/traverse.py
+++ py/cc3/traverse.py
@@ -1,8 +1,12 @@
 from collections import deque
+from .graph import ListGraph, MatrixGraph
 
 
 # BFS
-def bfs(graph, anchor = 0):
+def bfs(graph: ListGraph|MatrixGraph, anchor = 0):
+    return _bfs_list(graph, anchor) if isinstance(graph, ListGraph) else _bfs_matrix(graph, anchor)
+
+def _bfs_list(graph, anchor):
     queue = deque()
     visited = [False] * graph.order
     dist = [-1] * graph.order
@@ -21,14 +25,39 @@ def bfs(graph, anchor = 0):
 
     return dist
 
-# DFS
-def dfs(graph, anchor = 0):
+def _bfs_matrix(graph, anchor):
+    queue = deque()
     visited = [False] * graph.order
-    _dfs(graph, anchor, visited)
+    dist = [-1] * graph.order
+
+    queue.append(anchor)
+    visited[anchor] = True
+    dist[anchor] = 0
+
+    while len(queue) > 0:
+        current = queue.pop()
+        for e, w in enumerate(graph.matrix[current]):
+            if w != 0 and not visited[e]:
+                queue.append(e)
+                visited[e] = True
+                dist[e] = dist[current] + 1
+
+    return dist
+
+# DFS
+def dfs(graph: ListGraph|MatrixGraph, anchor = 0):
+    visited = [False] * graph.order
+    _dfs_list(graph, anchor, visited) if isinstance(graph, ListGraph) else _dfs_matrix(graph, anchor, visited)
     return visited
 
-def _dfs(graph, current, visited):
+def _dfs_list(graph, current, visited):
     visited[current] = True
     for e in graph.list[current]:
         if not visited[e.dest]:
-            _dfs(graph, e.dest, visited)
+            _dfs_list(graph, e.dest, visited)
+
+def _dfs_matrix(graph, current, visited):
+    visited[current] = True
+    for e, w in enumerate(graph.matrix[current]):
+        if w != 0 and not visited[e]:
+            _dfs_matrix(graph, e, visited)
