a69f0db4599c47e0b806b796b55569fbb951f621
jasononono <cdliaowendi@gmail.com>
2025-11-17 20:43:44 -0500

    toposort test

--- py/cc3/__init__.py
+++ py/cc3/__init__.py
@@ -1,6 +1,7 @@
 from .graph import *
 from .traversal import *
 from .cycle import *
+from .topo import *
 from .graphics import display
 
 
@@ -13,3 +14,28 @@ def new(variant = "list", *args, **kwargs) -> Graph | SuccessorGraph:
         return graph_variants[variant](*args, **kwargs)
 
     raise TypeError(f"no graph variant named '{variant}'")
+
+
+''' TODOS
+
+tortoise and hare
+tree functions on graphs (is_tree, is_forest, etc.)
+
+tree class with:
+    set_root, get_centers, get_diameter, get_radius, get_leaves
+    is_ancestor, is_child, is_parent, is_descendant
+    get_ancestors, get_children, get_parents, get_descendants
+
+toposort/ toposort_all functions
+    toposort_all should return a set of all unique topo-sorts of a graph
+    
+dependency function of Graph (does a node come before another node in the topo sort)
+
+count_islands / count_forests function
+graph to graph conversion / comparison function (change variant, from raw data, etc.)
+reverse all edges in directed graph
+behaviour with default operators (+, -, edge in graph, is, ==, etc.)
+
+the traversal boilerplate thingy oh no (final boss)
+
+'''
--- py/cc3/topo.py
+++ py/cc3/topo.py
@@ -1,24 +1,32 @@
 from collections import deque
+from collections.abc import Sequence
 from .graph import Graph, ListGraph, MatrixGraph, SuccessorGraph
 
 
-def sort(graph: Graph) -> Sequence[int]:
-    if isinstance(graph, ListGraph):
-        return _sort_list(graph)
-    if isinstance(graph, MatrixGraph):
-        return _sort_matrix(graph)
-    if isinstance(graph, SuccessorGraph):
-        return _sort_successor(graph)
-    
-    raise NotImplementedError(f"topological sorting not supported for '{type(graph).__name__}'")
+def topo_sort(graph: Graph, safe = False) -> Sequence[int]:
+    """topologically sort a directed graph into an array
 
-def _sort_list(graph: ListGraph) -> Sequence[int]:
+    if safe is True, then an unsortable graph (contains cycle) will raise an error."""
+
+    if isinstance(graph, ListGraph):
+        result = _topo_sort_list(graph)
+    elif isinstance(graph, MatrixGraph):
+        result = _topo_sort_list(graph)
+    elif isinstance(graph, SuccessorGraph):
+        result = _topo_sort_list(graph)
+    else:
+        raise NotImplementedError(f"topological sorting not supported for '{type(graph).__name__}'")
+
+    if safe and len(result) != graph.order:
+        raise ValueError("graph cannot be topologically sorted (contains cycle)")
+    return result
+
+def _topo_sort_list(graph: ListGraph) -> Sequence[int]:
     if not graph.directed:
         raise TypeError("topological sorting not supported on undirected graphs")
 
-    adj = graph.get_data()
     in_degree = [0] * graph.order
-    for n in graph:
+    for n in graph.adj:
         for e in n:
             in_degree[e.dest] += 1
 
@@ -28,11 +36,42 @@ def _sort_list(graph: ListGraph) -> Sequence[int]:
         if in_degree[n] == 0:
              queue.append(n)
 
-    while len(q) > 0:
+    while len(queue) > 0:
         current = queue.popleft()
         result.append(current)
 
-        for i in graph.adj[current]:
-            in_degree[i] -= 1
-            if in_degree[i] == 0:
-                queue.append(i)
+        for e in graph.adj[current]:
+            in_degree[e.dest] -= 1
+            if in_degree[e.dest] == 0:
+                queue.append(e.dest)
+
+    return result
+
+def _topo_sort_matrix(graph: MatrixGraph) -> Sequence[int]:
+    if not graph.directed:
+        raise TypeError("topological sorting not supported on undirected graphs")
+
+    in_degree = [0] * graph.order
+    for n in graph.adj:
+        for i, e in enumerate(n):
+            if e != graph.default_value:
+                in_degree[i] += 1
+
+    queue = deque()
+    result = []
+    for n in range(graph.order):
+        if in_degree[n] == 0:
+            queue.append(n)
+
+    while len(queue) > 0:
+        current = queue.popleft()
+        result.append(current)
+
+        for i, e in enumerate(graph.adj[current]):
+            if e != graph.default_value:
+                in_degree[i] -= 1
+                if in_degree[i] == 0:
+                    queue.append(i)
+
+    return result
+
